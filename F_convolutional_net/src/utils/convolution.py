6479768000a12e75a0ad2bef60d9570adbbdd9f2
lesimor <brillante91@naver.com>
2018-09-25 00:20:55 +0900

    wip

---
+++ F_convolutional_net/src/utils/convolution.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+__all__ = ['conv2d', 'max_pool_2x2']
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(input=x, filter=W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
